c2d0afd13cd10d60cf9c61a915d3fa1e1ae9a251
Mostafa Ashraf <m.ashraf.hegab@gmail.com>
2025-11-08 17:43:56 +0200

    fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
 export const API_URL =
-  import.meta.env.REACT_APP_API_URL ?? 'http://localhost:3000/api/graphql';
+  import.meta.env.VITE_APP_API_URL ?? 'http://localhost:3000/api/graphql';
 
 export const SERVER_URL =
-  import.meta.env.REACT_APP_SERVER_URL ?? 'http://localhost:3000';
+  import.meta.env.VITE_APP_SERVER_URL ?? 'http://localhost:3000';
 
 export const MAIN_LEAGUE = 'Double Dribble';
 export const MAIN_LEAGUE_ID = 'cmhpgxdk00000g5yptgkulhl1';
